1ba27a7c2b5e452bcf5ee5acc9319494f882fec0
Udvalkhorol <59464028+Udvalkhorol@users.noreply.github.com>
2022-04-22 23:46:23 +0800

    123

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import Button from "@mui/material/Button";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Cart from "./pages/Cart";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Marketplace from "./pages/Marketplace";
+import Order from "./pages/Order";
+import Product from "./pages/Product";
+import Register from "./pages/Register";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />;
+        <Route exact path="/marketplace" element={<Marketplace />} />;
+        <Route exact path="/product" element={<Product />} />;
+        <Route exact path="/cart" element={<Cart />} />;
+        <Route exact path="/order" element={<Order />} />;
+        <Route exact path="/login" element={<Login />} />;
+        <Route exact path="/register" element={<Register />} />;
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
